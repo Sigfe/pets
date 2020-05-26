f8623159c974ed725c6ee494f8f929db0c4207ca
Sigfe <64668588+Sigfe@users.noreply.github.com>
2020-05-26 19:47:31 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,10 +1,11 @@
 <?php
-//De Igor Felix Silva
   require_once'animais.php';
   require_once'ratel.php';
 
   $ratel = new ratel ("ratel", "quatro", "baixo", "texugo", "branca e preta");
 
-  $ratel 
-  $ratel -> raiva();
+  $ratel -> setreceber_dano("grande dano");
+  $ratel -> getreceber_dano();
+  $ratel -> setraiva("furioso");
+  $ratel -> getraiva();
 ?>
