f89c1254438b7d3faa6a0b0ce4d6dfa76a21e305
Sigfe <64668588+Sigfe@users.noreply.github.com>
2020-05-26 19:19:02 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,1 +1,9 @@
+<?php
+  require_once'animais.php';
+  require_once'ratel.php';
 
+  $ratel = new ratel ("texugo", "quatro", "baixo", "ratel", "branca e preta");
+
+  $ratel -> receber_dano();
+  $ratel -> raiva();
+?>
