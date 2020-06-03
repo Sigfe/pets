8f87f3abce1ba32f9f0f5f10a2153ce188645c3f
Sigfe <64668588+Sigfe@users.noreply.github.com>
2020-06-03 15:30:09 -0300

    Update main.php

--- main.php
+++ main.php
@@ -2,12 +2,19 @@
   //Feito por Igor Felix Silva
   require_once'animais.php';
   require_once'ratel.php';
+  require_once'boi.php';
 
-  $ratel = new ratel ("ratel", "quatro", "baixo", "texugo", "branca e preta");
+  $ratel = new ratel ("Ratel", "quatro", "baixo", "texugo", "branca e preta");
 
   $ratel -> setreceber_dano("grande dano");
   $ratel -> getreceber_dano();
   $ratel -> setraiva("furioso");
   $ratel -> getraiva();
+
+  $boi = new boi ("Boi", "quatro", "grande", "bovino", "preta");
+  
+  $boi -> setOld("envelhece");
+  $boi-> getOld();
+
+  //Feito por Igor Felix Silva
 ?>
-//Feito por Igor Felix Silva
